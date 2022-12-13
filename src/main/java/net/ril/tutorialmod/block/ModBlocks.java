ac54c9231ffb26af619392b8757b7b93a72cdc3c
TheAithr <theaithr@gmail.com>
2022-12-13 13:23:45 +1000

    Jumpy Block and Tanzanite Lamp Added

--- src/main/java/net/ril/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/ril/tutorialmod/block/ModBlocks.java
@@ -12,6 +12,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 import net.ril.tutorialmod.TutorialMod;
+import net.ril.tutorialmod.block.custom.JumpyBlock;
+import net.ril.tutorialmod.block.custom.TanzaniteLampBlock;
 import net.ril.tutorialmod.item.ModItemGroup;
 
 public class ModBlocks {
@@ -23,6 +25,12 @@ public class ModBlocks {
     public static final  Block DEEPSLATE_TANZANITE_ORE = registerBlock("deepslate_tanzanite_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(),
                     UniformIntProvider.create(3,  7)), ModItemGroup.Tanzanite);
+    public static final  Block JUMPY_BLOCK = registerBlock("jumpy_block",
+            new JumpyBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.Tanzanite);
+    public static final  Block TANZANITE_LAMP = registerBlock("tanzanite_lamp",
+            new TanzaniteLampBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()
+                    .luminance(state -> state.get(TanzaniteLampBlock.LIT) ? 15 : 0)), ModItemGroup.Tanzanite);
+
 
     private static Block registerBlock(String name, Block block, ItemGroup tab){
         registerBlockItem(name, block, tab);
--- src/main/java/net/ril/tutorialmod/item/custom/DiceItem.java
+++ src/main/java/net/ril/tutorialmod/item/custom/DiceItem.java
@@ -1,12 +1,18 @@
 package net.ril.tutorialmod.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 import java.util.Random;
 
 
@@ -26,6 +32,17 @@ public class DiceItem extends Item {
 
         return super.use(world, user, hand);
     }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()){
+            tooltip.add(Text.literal("Right click in main hand to roll the dice").formatted(Formatting.AQUA));
+        } else {
+            tooltip.add(Text.literal("Hold Shift for more Info").formatted(Formatting.YELLOW));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+
     private void outputRandomNumber(PlayerEntity player){
         player.sendMessage(Text.literal("The Dice Rolled " + getRandomNumber()));
     }
